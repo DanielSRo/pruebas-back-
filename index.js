ff0cc05495c73d1f298ad5adbff6f25b32491195
Dan SR <119873961+DanielSRo@users.noreply.github.com>
2025-01-28 16:37:11 -0500

    prueba #5

--- index.js
+++ index.js
@@ -1,116 +1,122 @@
-// const http = require('http')
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-app.use(cors())
+app.use(express.json())
 app.use(express.static('dist'))
-// *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-let notes = [
+app.use(cors())
+// app.use(morgan('tiny'))
+
+morgan.token('body', (req) => {
+  return req.body
+    ? JSON.stringify(req.body)
+    : 'No body';
+});
+
+app.use(morgan(':method :url :status :res[content-length] bytes - :response-time ms :body'));
+
+
+// *** *** *** *** *** *** *** *** *** *** *** ***
+let phoneBook = [
   {
     id: 1,
-    content: "HTML is easy",
-    important: true
+    name: "Arto Hellas",
+    number: "040-123456"
   },
   {
     id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
   }
 ]
+// *** *** *** *** *** *** *** *** *** *** *** ***
+const newId = () => {
+  const newId = phoneBook.length > 0
+    ? Math.max(...phoneBook.map(person => person.id)) + 1
+    : 1
 
-// *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
+  return newId
 }
 
-// *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+// *** *** *** *** *** *** *** *** *** *** *** ***
 
-// *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'application/json' })
-//   response.end(JSON.stringify(notes))
-// })
+app.get('/info', (request, response) => {
+  const entries = phoneBook.length
+  const date = new Date()
 
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!!</h1>')
+  response.send(
+    `
+    <h1>Informatio about the PhoneBook</h1>
+    <p>The phonebook has info for ${entries} ${entries > 1 ? 'persons' : 'person'}</p>
+    <p>Date of request: ${date}</p>
+    `
+  )
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/persons', (request, response) => {
+  response.json(phoneBook)
 })
 
-// *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-//Search a note for id 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(n => n.id === id)
+  const person = phoneBook.find(p => p.id === id)
 
-  if (note) {
-    response.json(note)
+  if (person) {
+    response.json(person)
   } else {
     response.status(404).end()
   }
 })
 
-// *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-//Delete a note for id 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  phoneBook = phoneBook.filter(n => n.id !== id)
 
   response.status(204).end()
 })
 
-// *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-//Create new note 
-app.use(express.json())
-app.use(requestLogger)
+app.post('/api/persons', (request, response) => {
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (!body.number) {
     return response.status(400).json({
-      error: 'content missing'
+      error: 'Missing Number'
+    })
+  } else if (!body.name) {
+    return response.status(400).json({
+      error: 'Missing Name'
+    })
+  } else if (phoneBook.find(n => n.name.toLowerCase() === body.name.toLowerCase())) {
+    return response.status(400).json({
+      error: `the name ${body.name} aldeary exists`
     })
   }
 
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
+  const person = {
+    id: newId(),
+    name: body.name,
+    number: body.number,
   }
 
-  notes = notes.concat(note)
+  phoneBook = phoneBook.concat(person)
 
-  response.json(note)
+  response.json(person)
 })
 
-// *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-app.use(unknownEndpoint)
 
-// *** *** *** *** *** *** *** *** *** *** *** *** *** ***
+// *** *** *** *** *** *** *** *** *** *** *** ***
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
